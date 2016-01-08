0f0990e749a540623b7276f57d0f5d2beb0309b0
fuzzybow <matt.seitter@gmail.com>
2016-01-08 13:14:32 -0500

    Update rulekick.pl

--- rulekick.pl
+++ rulekick.pl
@@ -3,23 +3,33 @@ use vars qw($VERSION %IRSSI);
 
 use Irssi;
 
+#########################################################################################################################################
+#If there are a lot of people breaking a rule, you can have a custom kick message preformated.						#
+#Use "/rkm [Rule]" to create a rule.													#
+#Then, use "/rk [user] [Spec]" to kick someone. The kick message will be the spec (specific) followed by the rule.			#
+#																	#
+#For example, if you had a rule against using adjectives, you would do "/rkm = adjective" which sets the rule to "= adjective"		#
+#Then, if somebody says pretty, you do "/rk nick pretty" and it will kick nick with the reason "pretty = adjective"			#
+#########################################################################################################################################
+
 $VERSION = '1.00';
 %IRSSI = (
     authors	=> 'fuzzybow',
     contact	=> 'fuzzybow@matte.one',
     name	=> 'RuleKick',
-    description	=> 'Simple Modifiable wordgame rulekick',
+    description	=> 'Simple Modifiable Rule Kick',
     license	=> 'MIT',
 );
 
-$rule = " = bad";
+my $rule = " ";
+my $server = shift;
 
 Irssi::command_bind rkm => sub {
-    $rule = $0;
+    $rule = $_[0];
 };
 
 Irssi::command_bind rk => sub {
-    $server->command("kick $0 $1 $rule");
+    my ($user, $freas, $server, $channel) = @_;
+    my ($reas, $v) = split('Irssi::Irc', $freas, 2);
+    $server->command("kick $user $reas $rule");
 };
-
-
